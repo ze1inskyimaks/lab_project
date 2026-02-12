f1222b5b172654ca2804f84e6ac5d36564e32f99
ze1inskyimaks <zelinskyimaks@gmail.com>
2026-02-12 22:05:56 +0200

    feat(name_tag): change name

--- src/main.js
+++ src/main.js
@@ -1,24 +1,12 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+const input = document.querySelector('#taskInput');
+const button = document.querySelector('#addBtn');
+const list = document.querySelector('#taskList');
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+button.addEventListener('click', () => {
+  const li = document.createElement('li');
+  li.textContent = input.value;
+  list.appendChild(li);
+  input.value = '';
+});
 
-setupCounter(document.querySelector('#counter'))
+//some texts
